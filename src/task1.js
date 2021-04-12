e805648dcaf6f18dbaf0bf91d93ca192021c1bb4
Christina <christina-hardy@live.nmit.ac.nz>
2021-04-13 10:22:28 +1200

    checking input task1.js

--- src/task1.js
+++ src/task1.js
@@ -1,8 +1,11 @@
 function addOddNumbers(number) {
+    if (typeof number !== 'number') {
+        return 'not a number'
+    }
     let result = 0
     for (let i = 1; i <= number; i += 2) {
         result += i;
     }
     return result;
 }
-console.log(addOddNumbers(13))
+console.log(addOddNumbers('ie'))
