1933307cc8b6407623319b55b43e2cbf0e865e0a
Christina <christina-hardy@live.nmit.ac.nz>
2021-04-13 09:55:17 +1200

    add comments to task2

--- src/task2.js
+++ src/task2.js
@@ -1,7 +1,11 @@
 function filterArray (array) {
+    // create empty array
     let result = []
+    //goes through each item in array and changes it into a number and pushes it into result
     array.forEach(x=>result.push(parseInt(x)))
+    //sorts the array into correct order
     result.sort()
+    //removes duplicate numbers and non value number
     return result.filter((item,index)=>result.indexOf(item)=== index)
 }
 console.log(filterArray([1,34, '99','a', '1']))
