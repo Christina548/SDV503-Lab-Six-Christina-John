1ced0ab8dd2cc8e62d7dab39b14b684ab44322d6
Christina <christina-hardy@live.nmit.ac.nz>
2021-04-13 10:40:41 +1200

    task3 changes

--- src/task3.js
+++ src/task3.js
@@ -1,12 +1,15 @@
 function getCount (object) { 
-    if (typeof object.likes!=='number'||typeof object.dislikes!=='number'){
+    let counts = object
+    let likes = parseInt(counts.likes)
+    let dislikes = parseInt(counts.dislikes)
+    if (!Number.isInteger(likes)||!Number.isInteger(dislikes)){
         return 'not a number'
     }
-    let difference = object.likes-object.dislikes;
+    let difference = counts.likes-counts.dislikes;
     if (difference>0){
         return difference+' likes'
     } else {
         return Math.abs(difference)+' dislikes'
     }
 }
-console.log(getCount({likes: 100, dislikes: 900}))
+console.log(getCount({likes: 1000, dislikes: 100}))
